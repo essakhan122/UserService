107171a4d441a27fb040dd521a93523997070d6a
ekhan122 <khanessa2@gmail.com>
2026-01-28 21:43:40 +0530

    added config from yaml

--- src/main/java/com/taskapp/userservice/config/PasswordConfig.java
+++ src/main/java/com/taskapp/userservice/config/PasswordConfig.java
@@ -1,5 +1,6 @@
 package com.taskapp.userservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,8 +9,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class PasswordConfig {
 
+    @Value("${security.password.bcrypt.strength:10}")
+    private int bcryptStrength;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(bcryptStrength);
     }
 }
